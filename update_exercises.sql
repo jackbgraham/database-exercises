cef70a8b712ef02f89bbd47f6a5f69ce37632de3
Jack Graham <jack.blair.graham@gmail.com>
2022-11-07 15:46:46 -0600

    missing semi colons

--- update_exercises.sql
+++ update_exercises.sql
@@ -6,14 +6,14 @@ USE codeup_test_db;
 SELECT * FROM albums;
 UPDATE albums
 SET sales = (sales *10);
-SELECT * FROM albums
+SELECT * FROM albums;
 
 #All albums released before 1980
 #Move all the albums before 1980 back to the 1800s.
 SELECT * FROM albums WHERE release_date <(1980);
 UPDATE albums
 SET release_date = (release_date -100);
-SELECT * FROM albums
+SELECT * FROM albums;
 
 #All albums by Michael Jackson
 #Change 'Michael Jackson' to 'Peter Jackson'
@@ -21,6 +21,6 @@ SELECT * FROM albums WHERE artist_name = 'Michael Jackson';
 UPDATE albums
 SET artist_name = 'Peter Jackson'
 WHERE artist_name = 'Michael Jackson';
-SELECT * FROM albums
+SELECT * FROM albums;
 
 
