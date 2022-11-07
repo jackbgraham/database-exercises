b5d719f40ed2b9be8c049a8f0a0bc6857a12646f
Jack Graham <jack.blair.graham@gmail.com>
2022-11-07 16:00:12 -0600

    albums migration

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    album_id INT NOT NULL AUTO_INCREMENT,
+    artist_name VARCHAR(30) NOT NULL,
+    album_name VARCHAR(50) NOT NULL,
+    release_date INT NOT NULL,
+    sales DOUBLE NOT NULL,
+    genre VARCHAR(50) NOT NULL,
+    PRIMARY KEY (album_id)
+);
